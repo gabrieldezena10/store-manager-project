5c76ad332fb7e243c721bc48a202df58db9cd6bf
gabrieldezena10 <gabriel.deztor@gmail.com>
2022-07-20 15:48:05 -0300

    Creating new repository

--- app.js
+++ app.js
@@ -9,12 +9,8 @@ app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/sales', salesRouter);
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
